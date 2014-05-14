b90438f74e791756411b582c52d5266d9ce493ba
Kacper Czechowicz <kipperek@gmail.com>
2014-05-14 22:09:18 +0200

    Gists controller tests

---
+++ spec/controllers/gists_controller_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+
+describe GistsController do
+	let(:valid_attributes) { { "lang" => "java", "description" => "some desc", "snippet" => "Some text" } }
+	let(:invalid_attributes) { { "lang" => "asdasdasdas", "description" => "some desc", "snippet" => "Some text" } }
+	let(:valid_session) { {} }
+
+	describe "GET index" do
+	    it "gets index and assign all gists into @gists" do
+	      gist = Gist.create! valid_attributes
+	      get :index, {}, valid_session
+	      expect(assigns(:gists)).to eq([gist])
+	    end
+	end
+
+	describe "GET new" do
+	    it "assigns blank gist as @gist" do
+	      get :new, {}, valid_session
+	      expect(assigns(:gist)).to be_a_new(Gist)
+	    end
+	end
+
+	describe "GET show" do
+	    it "assigns the requested gist as @gist" do
+	      gist = Gist.create! valid_attributes
+	      get :show, {:id => gist.to_param}, valid_session
+	      expect(assigns(:gist)).to eq(gist)
+	    end
+	 end
+
+	describe "GET edit" do
+	    it "assigns the requested gist as @gist" do
+	      gist = Gist.create! valid_attributes
+	      get :edit, {:id => gist.to_param}, valid_session
+	      expect(assigns(:gist)).to eq(gist)
+	    end
+	  end
+
+	describe "#Create new gist" do
+		it "creates a new Gist" do
+			User.create
+			session[:user_id] = 1
+
+	        expect {
+	          post :create, {:gist => valid_attributes}, valid_session
+	        }.to change(Gist, :count).by(1)
+     	end
+	
+		it "gists with valid attr have valid lang" do
+     		User.create
+			session[:user_id] = 1
+
+			post :create, {:gist => valid_attributes}, valid_session
+
+			expect(assigns[:gist].lang).to eq(valid_attributes["lang"])
+     	end
+
+     	it "gists with invalid attr have lang: text" do
+     		User.create
+			session[:user_id] = 1
+
+			post :create, {:gist => invalid_attributes}, valid_session
+
+			expect(assigns[:gist].lang).to eq("text")
+     	end
+	end
+
+	describe "destroy" do
+		it "destroys the requested gist" do
+		  User.create
+		  session[:user_id] = 1
+	      gist = Gist.create! valid_attributes
+	      expect {
+	        delete :destroy, {:id => gist.to_param}, valid_session
+	      }.to change(Gist, :count).by(-1)
+    	end
+	end
+
+end
