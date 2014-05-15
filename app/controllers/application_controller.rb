5f6de6faf311c258d7f3cf2c847934ebda3604fd
Patryk <patlaw96@gmail.com>
2014-05-16 00:43:08 +0200

    fixed controllers and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,6 @@ class ApplicationController < ActionController::Base
     end
   end
  
-  private
- 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
