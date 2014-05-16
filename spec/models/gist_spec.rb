ca36875f68030fc24463309788f9a10a1e7a1da7
kuba <jakubplenis@gmail.com>
2014-05-16 08:45:56 +0200

    Starting Part of Testing Models

---
+++ spec/models/gist_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+
+
+describe Gist do
+	it "create gist" do
+      gist = Gist.create
+    end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Post do
+    it "create post" do
+      post = Post.create
+    end
+end
