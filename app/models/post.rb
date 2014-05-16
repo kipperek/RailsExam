352ec7abe731d0cd966053d28ad2099967b9da0c
mossowski <lcinek18@gmail.com>
2014-05-16 16:39:58 +0200

    layout changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
 	 belongs_to :user
+
+def timestamp
+  created_at.strftime('%d %B %Y %H:%M:%S')
+end
+
 def self.search(search)
     if search 
         find(:all, :conditions => ['content  LIKE ?',"%#{search}%"])
