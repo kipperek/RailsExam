d48c573a58dd8af222b8344543ba129144b9e534
kuba <jakubplenis@gmail.com>
2014-05-16 21:03:39 +0200

    Search by ID_user added

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -2,7 +2,7 @@ class Gist < ActiveRecord::Base
 	 belongs_to :user
 def self.search(search)
     if search 
-        find(:all, :conditions => ['description OR snippet LIKE ?', "%#{search}%"])
+        find(:all, :conditions => ['description OR snippet OR user_id LIKE ?', "%#{search}%"])
     else
         all
     end
