46e6980bbb4ad33f8f87482125d8e5d4bfe2289c
Patryk <patlaw96@gmail.com>
2014-05-18 00:29:29 +0200

    Update gist.rb

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -5,7 +5,7 @@ class Gist < ActiveRecord::Base
 	
 def self.search(search)
     if search 
-        @f = find(:all, :conditions => ['description  LIKE ?', '%#{search}%'])
+        find(:all, :conditions => ['description  LIKE ?', '%#{search}%'])
     else
         all
     end
