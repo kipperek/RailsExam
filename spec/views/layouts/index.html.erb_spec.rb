cdb3c7d8e0bfc1df0fece74438b28fb71900cb8c
Patryk <patlaw96@gmail.com>
2014-05-17 16:32:57 +0200

    layout test

---
+++ spec/views/layouts/index.html.erb_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+def current_user 
+  return User.create({:is_admin => true, :name => "Nowak"})
+end
+
+def name
+  return "Nowak"
+end
+
+describe "layouts/application" do
+
+  it "check login name label" do
+    render
+    assert_select "a", :text => 'Nowak', :count => 1
+  end
+end
