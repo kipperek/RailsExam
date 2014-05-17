8f16c7d46f804dff51b65db80ced8df2654abb23
kuba <jakubplenis@gmail.com>
2014-05-17 21:59:08 +0200

    Login by GITHUB changed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, "41810262d4ccdbd905dc", "a7b3ee878d2ce07375debe7c0265a367445ea76d"
 end
