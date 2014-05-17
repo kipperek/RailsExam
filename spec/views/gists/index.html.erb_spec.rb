1824704d397b63d327ee6f00cc7c7d33356d664d
Patryk <patlaw96@gmail.com>
2014-05-17 17:19:57 +0200

    gists/index buttons tests

--- spec/views/gists/index.html.erb_spec.rb
+++ spec/views/gists/index.html.erb_spec.rb
@@ -54,3 +54,143 @@ describe "gists/index" do
     assert_select "a", :text => 'Show', :count => 2
   end
 end
+
+describe "gists/index" do
+    before(:each) do
+    assign(:gists, Kaminari.paginate_array(
+          [
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            ),
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            )
+          ]).page(1)
+    )
+  end
+
+  it "check if there is a edit button after login" do
+    @usr = User.create
+    session[:user_id] = @usr.id
+    render
+    assert_select "a", :text => 'Edit', :count => 2
+  end
+end
+
+describe "gists/index" do
+    before(:each) do
+    assign(:gists, Kaminari.paginate_array(
+          [
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            ),
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            )
+          ]).page(1)
+    )
+  end
+
+  it "check if there is a destroy button after login" do
+    @usr = User.create
+    session[:user_id] = @usr.id
+    render
+    assert_select "a", :text => 'Destroy', :count => 2
+  end
+end
+
+describe "gists/index" do
+    before(:each) do
+    assign(:gists, Kaminari.paginate_array(
+          [
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            ),
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            )
+          ]).page(1)
+    )
+  end
+
+  it "check if there is a new gist button after login" do
+    @usr = User.create
+    session[:user_id] = @usr.id
+    render
+    assert_select "a", :text => 'New Gist', :count => 1
+  end
+end
+
+describe "gists/index" do
+    before(:each) do
+    assign(:gists, Kaminari.paginate_array(
+          [
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            ),
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            )
+          ]).page(1)
+    )
+  end
+
+  it "check if there is a my gists button after login" do
+    @usr = User.create
+    session[:user_id] = @usr.id
+    render
+    assert_select "a", :text => 'My gists', :count => 1
+  end
+end
+
+describe "gists/index" do
+    before(:each) do
+    assign(:gists, Kaminari.paginate_array(
+          [
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            ),
+            stub_model(Gist,
+              :snippet => "main",
+              :lang => "text",
+              :description => "i like ruby",
+              :user_id => "1"
+            )
+          ]).page(1)
+    )
+  end
+
+  it "check if there is a all gists button after login" do
+    @usr = User.create
+    session[:user_id] = @usr.id
+    render
+    assert_select "a", :text => 'All gists', :count => 1
+  end
+end
