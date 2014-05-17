500d44e59ec4bae8b6707af5d7ac4ab45022303e
Patryk <patlaw96@gmail.com>
2014-05-18 00:04:38 +0200

    Update gist.rb

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -5,7 +5,7 @@ class Gist < ActiveRecord::Base
 	
 def self.search(search)
     if search 
-        find(:all, :conditions => ['description  LIKE ? OR snippet LIKE ?', "%#{search}%"])
+        find(:all, :conditions => ['description  LIKE %#{search}% OR snippet LIKE ?', "%#{search}%"])
     else
         all
     end
