21148708d0cdf29e164ac634cd283a469bdc480f
kuba <jakubplenis@gmail.com>
2014-05-18 00:40:00 +0200

    one more try

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -5,7 +5,7 @@ class Gist < ActiveRecord::Base
 	
 def self.search(search)
     if search 
-        find(:all, :conditions => ['description  LIKE ?', '%#{search}%'])
+        find(:all, :conditions => ['snippet  LIKE ?', '%#{search}%'])
     else
         all
     end
