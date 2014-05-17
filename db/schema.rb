ab2117482fc6308a95810ff53f4a35ac1a1e190f
Patryk <patlaw96@gmail.com>
2014-05-17 16:55:27 +0200

    new post button test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515175053) do
+ActiveRecord::Schema.define(version: 20140513222022) do
 
   create_table "gists", force: true do |t|
     t.text     "snippet"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140515175053) do
 
   create_table "posts", force: true do |t|
     t.text     "content"
+    t.string   "date"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -41,7 +42,6 @@ ActiveRecord::Schema.define(version: 20140515175053) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nick"
-    t.boolean  "is_admin"
   end
 
 end
--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -19,8 +19,39 @@ describe "posts/index" do
   end
 
   it "renders list of posts" do
+
     render
     assert_select "div>div>div>p", :text => "I like ruby !".to_s, :count => 2
     assert_select "div>div>div>h3", :text => "MahPost".to_s, 	  :count => 2
   end
 end
+
+describe "posts/index" do
+  before(:each) do
+    assign(:posts, Kaminari.paginate_array(
+      [
+        stub_model(Post,
+          :content => "I like ruby !",
+          :description => "MahPost",
+                :timestamp => "17 May 2014 13:40:54",
+                :user_id => "1"
+        ),
+        stub_model(Post,
+          :content => "I like ruby !",
+          :description => "MahPost",
+                :timestamp => "17 May 2014 13:40:54",
+                :user_id => "1"
+        )
+      ]).page(1)
+    )
+  end
+
+  it "check if admin see add post button" do
+    @usr = User.create({:is_admin => true})
+    session[:user_id] = @usr.id
+    render
+    assert_select "a", :text => "New Post".to_s, :count => 1
+
+  end
+end
+
