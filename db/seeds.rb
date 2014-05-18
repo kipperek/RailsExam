641773f655101483024651ad4d7f848a20e9dba6
kuba <jakubplenis@gmail.com>
2014-05-18 02:06:41 +0200

    SEEDS Changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-for i in 0..50
-Gist.create(snippet:'#include <stdio.h>
-int main()
-{
-int a = 22;
-int b = 12;
+for i in 0..10
+Gist.create(snippet:'
+# Output "I love Ruby"
+say = "I love Ruby"
+puts say
+# Output "I *LOVE* RUBY"
+say["love"] = "*love*"
+puts say.upcase
+# Output "I *love* Ruby"
+# five times
+5.times { puts say }
+', lang:'ruby',description:'I love Ruby '+i.to_s)
 
-printf("%i",a*b);
+Gist.create(snippet:'
+import javax.swing.JOptionPane;
+public class HelloWorld {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, \"Hello, world!\");
+}
+}
+', lang:'java',description:'I love Java '+i.to_s)
 
-return 0;
-}', lang:'c',description:'C '+i.to_s)
+Gist.create(snippet:'
+class Emulator:
+ 
+def __call__(self, x):
+        print "Ten Emulator", x
+ 
+def __getitem__(self, key):
+return str(key) + "Emulator"
+ 
+e = Emulator()
+e(abc)
+e(123)
+print e[def]
+print e[456]
+', lang:'python',description:'I love Python '+i.to_s)
+
+Gist.create(snippet:'
+mov ax, 0D625h
+mov es, ax       
+mov al, 24
+mov ah, 0        
+int 21h          
+', lang:'DASM16',description:'I love Asm '+i.to_s)
+
+Gist.create(snippet:'
+<script type="text/javascript">
+/* <![CDATA[ */
+ alert(12 > 6);
+/*]]> */</script>
+', lang:'js',description:'I love Javascript '+i.to_s)
 end
 
-for i in 0..50
-Post.create(content:'Projekt zespolowy w Ruby :)',description:'Post '+i.to_s)
+for i in 0..10
+Post.create(content:'Aplikacja została wdrożona na shellycloud',description:'Post '+i.to_s)
+Post.create(content:'Aplikacja została stworzona przez zespół CŁOP',description:'Post '+i.to_s)
+Post.create(content:'Zyczymy milego dnia',description:'Post '+i.to_s)
+
 end
+
